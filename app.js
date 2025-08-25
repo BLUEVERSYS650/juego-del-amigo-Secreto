61556c2b77e8725af3ee45a7af6a11e97f8b8e9a
Diego <131321132+BLUEVERSYS650@users.noreply.github.com>
2025-08-25 12:28:59 -0400

    Update app.js
    Version 1

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/let inputName = document.getElementById("amigo");
+let listaAmigos = [];
+let ulListaAmigos = document.getElementById("listaAmigos");
+let ulresultado = document.getElementById("resultado");
+//let amigoSorteado = miLista.shift();
+
+function agregarAmigo(){
+    if(inputName.value){
+         alert("gracias por participar!!!");
+        //ulListaAmigos.innerHTML = "gracias por participar!!!";
+    }else{
+       alert("Por favor ingresa tu nombre, no seas aguafiestas!!!");
+    } 
+    listaAmigos.push(inputName.value);
+    //console.log(listaAmigos);
+    ulListaAmigos.innerHTML += `<li>${inputName.value}</li>`;
+    limpiarCaja();
+    return
+    
+}   
+
+function limpiarCaja() {
+    inputName.value = ""; // Limpiamos el input
+
+} 
+    
+    function sortearAmigo(){
+    let random = Math.floor(Math.random() * listaAmigos.length);
+    //alert(`Tu amigo secreto es: ${listaAmigos[random]}`);
+    let amigoSecreto = listaAmigos[random];
+    ulresultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`;
+    listaAmigos.splice(random, 1);
+    return
+    
+}
+
+
+
