570368f8728919fe491f2ffa0fc8d01f9a712f2a
BLUEVERSYS650 <d.rosales24.dr@gmail.com>
2025-09-04 21:25:21 -0400

    version final

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 primer codigo de javasscript
 console.log("vamos que se puede");
 
 se modifico el en app.js el alert, se cambio alert por mensajes en <h2> con temporizador
+=======
+challenger amigo secreto
+
+se crea app.js para funcionamiento del html
+
+se modifico los alert por textos en <h2>
+
+
+>>>>>>> 635d26d (se agrego function limpiar lista)
--- app.js
+++ app.js
@@ -2,11 +2,21 @@ let inputName = document.getElementById("amigo");
 let listaAmigos = [];
 let ulListaAmigos = document.getElementById("listaAmigos");
 let ulresultado = document.getElementById("resultado");
+let amigoSorteado = "";
 
-function condicionesIniciales(){
-    asignarTextoElemento('h2', 'Hola, escribe tu nombre y apellido');
+
+function mensajeInicial(){
+    asignarTextoElemento('h2', 'Hola, escribe tu nombre.');
 }
 
+function limpiarLista(){//limpia lista de amigos y resultado
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
+    listaAmigos = [];
+    amigoSorteado = "";
+    return;
+
+}
 
 function limpiarCaja() {
     inputName.value = ""; // Limpiamos el input
@@ -21,47 +31,43 @@ function asignarTextoElemento(elemento, texto) {
 
 function agregarAmigo(){
     if(inputName.value){
-        asignarTextoElemento('h2', 'Gracias por participar!!!');
+        asignarTextoElemento('h2', 'Gracias por jugar!!!');
         //alert("gracias por participar!!!");
         listaAmigos.push(inputName.value); 
         ulListaAmigos.innerHTML += `<li>${inputName.value}</li>`;
         limpiarCaja();
-        
+       
     }else{
        asignarTextoElemento('h2', 'Ingresa tu nombre, no seas Aguafiestas!!!');
         //alert("Por favor ingresa tu nombre, no seas aguafiestas!!!");
     }
-    setTimeout(condicionesIniciales, 2000);
+    setTimeout(mensajeInicial, 2000);
     return
+
       
 }  
-
-
 
 function sortearAmigo() {
   if (listaAmigos.length === 0) {
     asignarTextoElemento('h2', 'ya fueron todos sorteados, no hay mas amigos.');
-    ulresultado.innerHTML = ''; 
+    ulresultado.innerHTML = '';
+    limpiarLista();
+    setTimeout(mensajeInicial, 2000);
     return;
-
   }
 
   let random = Math.floor(Math.random() * listaAmigos.length);
   let amigoSecreto = listaAmigos[random];
-  ulresultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`;
+  ulresultado.innerHTML = `<li>tu amigo secreto : ${amigoSecreto} !!!</li>`;
   listaAmigos.splice(random, 1);
   limpiarCaja();
-  asignarTextoElemento('h2', 'Gracias por participar, compra un lindo regalo!!!');
-  setTimeout(condicionesIniciales, 2500);
+  asignarTextoElemento('h2', 'Comprale un lindo regalo a:');
+  setTimeout(mensajeInicial, 2000);
   return;
+  
 }
 
-function reiniciarJuego(){
-    limpiarCaja();
-    condicionesIniciales();
-}
-
-condicionesIniciales();
+mensajeInicial();
 
 
 
